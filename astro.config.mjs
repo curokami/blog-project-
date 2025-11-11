72c8473fe054565cafcc29f64461cc06045940e3
curokami <berettaxx@gmail.com>
2025-11-11 18:17:18 +0900

    fix: Add vite config to externalize node modules and fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,15 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  integrations: [preact(), tailwind()]
+  integrations: [preact(), tailwind()],
+  vite: {
+    ssr: {
+      external: ['node:url'],
+    },
+    build: {
+      rollupOptions: {
+        external: ['node:url', 'clean-stack'],
+      },
+    },
+  },
 });
