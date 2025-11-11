8f95112c5e49103fb5d1820540589dadd340833e
curokami <berettaxx@gmail.com>
2025-11-11 23:37:19 +0900

    fix: Prevent multiple CMS initializations

--- src/lib/cms.ts
+++ src/lib/cms.ts
@@ -3,7 +3,14 @@ import CMS from "decap-cms-app";
 import { CustomAuth } from "./auth";
 import config from "./config";
 
+let initialized = false;
+
 export function initCMS() {
+  if (initialized) {
+    return;
+  }
+  initialized = true;
+
   CMS.registerBackend("github", CustomAuth);
   CMS.init({ config });
 }
